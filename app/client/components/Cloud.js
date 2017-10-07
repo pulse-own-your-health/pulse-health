0937250f6f658d06257031edcb19f7a1a265eff2
vauhochzett <vauhochzett@users.noreply.github.com>
2017-10-07 21:49:36 +0200

    Adds stub cloud files

---
+++ app/client/components/Cloud.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Valentin on 07/10/2017.
+ */
+
+import React from 'react';
+import './Cloud.css';
+
+export class Cloud extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>HELLO</h1>
+      </div>
+    );
+  };
+}
