9a0a63edc687e34b17d674313eb9e2a699676506
OndragKlaus <OndragKlaus@users.noreply.github.com>
2017-10-07 16:19:50 +0200

    added dummy circle component

--- app/client/routes.js
+++ app/client/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import { App } from './components/App.js';
 import { Main } from './components/Main.js';
 import { NotFound } from './components/NotFound.js';
+import { Circle } from "./components/Circle";
 
 export const routes = (
   <Route path='/' component={App}>
     <IndexRoute title='App' component={Main} />
+    <Route path='circle' title='Circle' component={Circle} />
     <Route path='*' title='404: Not Found' component={NotFound} />
   </Route>
 );
